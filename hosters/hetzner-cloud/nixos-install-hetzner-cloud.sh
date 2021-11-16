b8a38aefb02f68d9eafd98132080719605b53943
ragon000 <me@phochkamp.de>
2021-11-16 14:45:38 +0100

    Update nixos-install-hetzner-cloud.sh

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -66,7 +66,7 @@ echo '
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-rsa ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJwOH1b6xWmEr1VZh48kBIYhW11vtPFR3my8stAHlSi"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
